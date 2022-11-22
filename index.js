4183e52f12274fc54b5a03036fc3535785532044
Happiness-James <gesarehjames@gmail.com>
2022-11-22 18:50:39 +0000

    commit tree a0466f3862d77c58e54154a18db8f961438585a6

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Happiness James';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}${currentUser} !`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+console.log(shortGreeting)
+
